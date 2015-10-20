d389535b58a3c1fec5923ddd5dd3e8b7429030da
SSupattapone <samuelsupattapone@hanovernorwichschools.org>
2015-10-20 14:26:26 -0400

    Updated from Brython Server: 10/20/2015 2:26:28 PM

--- distribution.py
+++ distribution.py
@@ -39,7 +39,10 @@ Notice about this example:
 text = input("Please enter a string of text (the bigger the better): ")
 print(('The distribution of characters in "{0}" is: ').format(text))
 
-
+Ltext = str(text).lower()
 
 dist = []
 alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+for x in alpha:
+    

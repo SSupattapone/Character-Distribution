5dcee8d0eb01eb1fb800b210cacf78dba6f43130
SSupattapone <samuelsupattapone@hanovernorwichschools.org>
2015-10-26 14:54:53 -0400

    Updated from Brython Server: 10/26/2015 2:55:02 PM

--- distribution.py
+++ distribution.py
@@ -37,7 +37,7 @@ Notice about this example:
 * Letters that do not occur in the text are not listed in the output at all.
 """
 text = input("Please enter a string of text (the bigger the better): ")
-print(('The distribution of characters in "{0}" is: ').format(text))
+print(('The distribution of characters in "{0}" is:').format(text))
 
 Ltext = str(text).lower()
 maxlen = len(text)
